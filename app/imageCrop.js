09b534b36e32f6df61dc1c2a4f8839143e758cef
PUSELETS <dimamabolo15@gmail.com>
2023-12-23 09:17:24 +0000

    ko

--- app/imageCrop.js
+++ app/imageCrop.js
@@ -1,6 +1,6 @@
 "use client"
 import React from 'react';
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { useGesture } from '@use-gesture/react';
 
 export default function ImageCrop({ src }) {
@@ -17,40 +17,16 @@ export default function ImageCrop({ src }) {
             setCrop((crop) => ({ ...crop, scale: 1 + d / 50 }));
         },
 
-        onDragEnd: () => {
-            const newCrop = crop;
-            const imageBounds = imageRef.current.getBoundingClientRect();
-            const containerBounds = imageContainerRef.current.getBoundingClientRect();
-            const originalWidth = imageRef.current.clientWidth;
-            const widthOverhang = (imageBounds.width - originalWidth) / 2;
-            const originalHeight = imageRef.current.clientHeight;
-            const heightOverhang = (imageBounds.width - originalHeight) / 2;
-
-            if(imageBounds.left > containerBounds.left) {
-                newCrop.x = widthOverhang;
-            } else if (imageBounds.right < containerBounds.right){
-                newCrop.x = -(imageBounds.width - containerBounds.width) + widthOverhang;
-            }
-
-            if(imageBounds.top > containerBounds.top) {
-                newCrop.y = heightOverhang;
-            }else if (imageBounds.bottom < containerBounds.bottom){
-                newCrop.y = (imageBounds.height - containerBounds.height) + heightOverhang;
-            }
-
-            setCrop(newCrop);
-        },
+        
 
     }, {
-        drag: {
-            from: ()=>[crop.x, crop.y]
-        },
         pinch: {
             distanceBounds: {min: 0},
         },
         target: imageRef,
         eventOptions: { passive: false },
     });
+    
 
 
     return (
