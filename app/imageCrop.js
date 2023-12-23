e6b72937ad94f4236a387859993e55c013b4c19b
PUSELETS <dimamabolo15@gmail.com>
2023-12-23 05:57:05 +0000

    x

--- app/imageCrop.js
+++ app/imageCrop.js
@@ -5,8 +5,9 @@ import { useGesture } from '@use-gesture/react';
 
 export default function ImageCrop({ src }) {
 
-    const [crop, setCrop] = useState({ x: 0, y: 0, scale: 2 })
-    const imageRef = useRef()
+    const [crop, setCrop] = useState({ x: 0, y: 0, scale: 1 });
+    const imageRef = useRef();
+    const imageContainerRef = useRef();
 
     useGesture({
         onDrag: ({ offset: [dx, dy] }) => {
@@ -15,6 +16,18 @@ export default function ImageCrop({ src }) {
         onPinch: ({ offset: [d] }) => {
             setCrop((crop) => ({ ...crop, scale: 1 + d / 50 }));
         },
+
+        onDragEnd: () => {
+            const newCrop = crop;
+            const imageBounds = imageRef.current.getBoundingClientRect();
+            const containerBounds = imageContainerRef.current.getBoundingClientRect();
+
+            if(imageBounds.left > containerBounds.left) {
+                newCrop.x = 0;
+            }
+            setCrop(newCrop);
+        },
+
     }, {
         target: imageRef,
         eventOptions: { passive: false },
@@ -24,18 +37,18 @@ export default function ImageCrop({ src }) {
     return (
         <>
             <div className='overflow-hidden aspect-[1]'>
-                <div>
-                <img
-                src={src}
-                ref={imageRef}
-                style={{
-                    left: crop.x,
-                    top: crop.y,
-                    transform: `scale(${crop.scale})`,
-                    touchAction: "none",
-                }}
-                className='relative object-contain w-full h-full max-w-none max-h-none'
-                />
+                <div ref={imageContainerRef}>
+                    <img
+                        src={src}
+                        ref={imageRef}
+                        style={{
+                            left: crop.x,
+                            top: crop.y,
+                            transform: `scale(${crop.scale})`,
+                            touchAction: "none",
+                        }}
+                        className='relative object-contain w-full h-full max-w-none max-h-none'
+                    />
                 </div>
             </div>
         </>
