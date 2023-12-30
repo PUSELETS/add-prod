eb293078b2168d519f5b12f4bbfb23cde42ebdc3
PUSELETS <dimamabolo15@gmail.com>
2023-12-30 09:04:19 +0000

    correct

--- app/imageCrop.js
+++ app/imageCrop.js
@@ -108,7 +108,7 @@ export default function ImageCrop({ src, crop, onCropChange }) {
                             scale: scale,
                             touchAction: "none",
                         }}
-                        className='relative object-cover w-full h-full max-w-none max-h-none'
+                        className='relative w-full h-full max-w-none max-h-none'
                     />
                 </div>
             </div> 
